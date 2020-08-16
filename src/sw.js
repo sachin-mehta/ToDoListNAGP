3c210a8582a2efb104d5ec7dcb075a6fed300182
sachinmehta <sachin.mehta@nagarro.com>
2020-08-16 23:52:45 +0530

    AddedFiles
    Added code

---
+++ src/sw.js
@@ -0,0 +1,21 @@
+var cacheName = 'todo-pwa'; 
+var filesToCache = [
+'/',    
+'/index.html',    
+'/todo-widget.min.js',
+'/icon.png',
+'/manifest.json'  ];  
+self.addEventListener('install', function(e) { 
+e.waitUntil(
+caches.open(cacheName).then(function(cache) { 
+return cache.addAll(filesToCache);   
+})    
+);  
+}); 
+/* Serve cached content when offline */ 
+self.addEventListener('fetch', function(e) {  
+e.respondWith(      caches.match(e.request).then(function(response) {  
+return response || fetch(e.request);
+})   
+);  
+});
