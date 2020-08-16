da61a37555e6205d41e2d21e4a9f05e24a5de745
sachinmehta <sachin.mehta@nagarro.com>
2020-08-17 00:09:29 +0530

    ChangedStructure

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import TodoList from './lib/todo-list';
-import ListItem from './lib/list-item';
-import "./lib/main";
-import "./css/style.css";
-
-customElements.define('list-item', ListItem);
-customElements.define('todo-list', TodoList);
